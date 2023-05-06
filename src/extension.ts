0dec007b7b0742b29601112496ca9b6bc15950c6
Syncher Pylon <Syncviip@gmail.com>
2023-05-06 22:32:42 +0800

    fix engines

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,12 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('v-swagger is activated');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "v-swagger" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('v-swagger.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('v-swagger.preview', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from v-swagger!');
+		vscode.window.showInformationMessage('Pylon from v-swagger!');
 	});
 
 	context.subscriptions.push(disposable);
