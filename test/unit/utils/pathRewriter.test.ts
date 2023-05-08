4f69c06e4f76d6ea2c025156567d3dd96910526a
Syncher Pylon <Syncviip@gmail.com>
2023-05-08 20:02:25 +0800

    feat: rename

--- test/unit/utils/pathRewriter.test.ts
+++ test/unit/utils/pathRewriter.test.ts
@@ -5,18 +5,18 @@ import { PathRewriter } from '../../../src/utils/pathRewriter';
 describe('test pathRewriter', () => {
     it('should apply rewrite rules correctly', async () => {
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        const vPathRewriter = new PathRewriter({ '@sap/deepsea-catalog-shared': '../../catalog-shared' });
-        expect(vPathRewriter.isApplicable()).toBeTruthy();
+        const pathRewriter = new PathRewriter({ '@sap/deepsea-catalog-shared': '../../catalog-shared' });
+        expect(pathRewriter.isApplicable()).toBeTruthy();
         const resolvedPath = path.resolve(__dirname, '../../../../test/unit/utils');
         const inputText = await (await readFile(path.join(resolvedPath, 'input.yaml'))).toString();
         const expectedText = await (await readFile(path.join(resolvedPath, 'output.yaml'))).toString();
-        const replacedText = vPathRewriter.rewrite(inputText);
+        const replacedText = pathRewriter.rewrite(inputText);
 
         expect(replacedText).toBe(expectedText);
     });
 
     it('path rewrite is not applicable', () => {
-        const vPathRewriter = new PathRewriter({});
-        expect(vPathRewriter.isApplicable()).toBeFalsy();
+        const pathRewriter = new PathRewriter({});
+        expect(pathRewriter.isApplicable()).toBeFalsy();
     });
 });
